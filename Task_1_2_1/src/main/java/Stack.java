5fdf2eb61453798d8d45312f4bdb84437430ae8b
MZolot <mzolotareva1609@gmail.com>
2021-10-15 16:03:56 +0700

    Все вроде работает, тесты есть

--- Task_1_2_1/src/main/java/Stack.java
+++ Task_1_2_1/src/main/java/Stack.java
@@ -1,28 +1,28 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Stack<T> {
 
     public T[] stackElements;
     private int stackLen;
-    private int stackCapacity;
+    private final Class<T> stackClass;
 
-    @SuppressWarnings("unckeched")
-    public Stack() {
-        this.stackCapacity = 10;
-        this.stackElements = (T[]) new Object[stackCapacity];
+    public Stack(Class<T> cl) {
+        this.stackElements = (T[]) Array.newInstance(cl, 10);
+        //this.stackElements = (T[]) new Object[10];
         this.stackLen = 0;
+        this.stackClass = cl;
     }
 
     private void stackExpand(int newCap) {
         T[] newStack;
         newStack = Arrays.copyOf(stackElements, newCap);
-        stackCapacity = newCap;
         stackElements = newStack;
     }
 
     public void push(T x) {
-        if (stackLen == stackCapacity) {
-            stackExpand(stackCapacity * 2);
+        if (stackLen == stackElements.length) {
+            stackExpand(stackElements.length * 2);
         }
         stackElements[stackLen++] = x;
     }
@@ -31,12 +31,12 @@ public class Stack<T> {
         if (st == null) {
             throw new IllegalArgumentException("Trying to push null stack.");
         }
-        int stLen = st.count();
+        int stLen = st.size();
         if (stLen == 0) {
             return;
         }
-        if (stackLen + stLen >= stackCapacity) {
-            stackExpand(stackCapacity + stLen + 1);
+        if (stackLen + stLen >= stackElements.length) {
+            stackExpand(stackElements.length + stLen + 1);
         }
         System.arraycopy(st.stackElements, 0, stackElements, stackLen, stLen);
         stackLen += stLen;
@@ -47,19 +47,20 @@ public class Stack<T> {
             throw new IndexOutOfBoundsException("Trying to extract elements from empty stack.");
         }
         stackLen--;
-        return stackElements[stackLen];
+        T res = stackElements[stackLen];
+        stackElements[stackLen] = null;
+        return res;
     }
 
-    public int count() {
+    public int size() {
         return stackLen;
     }
-
 
     public Stack<T> popStack(int len) {
         if (stackLen < len || len < 0) {
             throw new IllegalArgumentException("Incorrect elements amount.");
         }
-        Stack<T> res = new Stack<>();
+        Stack<T> res = new Stack<>(stackClass);
         for (int i = (stackLen - len); i < stackLen; i++) {
             res.push(stackElements[i]);
         }
