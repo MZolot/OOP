d30c6489e864ea31125d5bcf32d85473e0b78777
MZolot <mzolotareva1609@gmail.com>
2021-11-19 22:50:57 +0700

    Внесла часть правок

--- Task_1_4_1/src/main/java/Calculator.java
+++ Task_1_4_1/src/main/java/Calculator.java
@@ -2,18 +2,6 @@ import java.util.Scanner;
 
 public class Calculator {
 
-     public static double getArg(Scanner scanner) {
-        if (!scanner.hasNext()) {
-            throw new IllegalArgumentException("Not enough arguments");
-        }
-        if (scanner.hasNextDouble()) {
-            return scanner.nextDouble();
-        } else {
-            Operations operation = operationsFactory(scanner.next());
-            return operation.calculate(scanner);
-        }
-    }
-
     public double calculate(Scanner scanner) {
         if (!scanner.hasNext()) {
             throw new IllegalArgumentException("Empty input");
@@ -21,21 +9,21 @@ public class Calculator {
         if (scanner.hasNextDouble()) {
             throw new IllegalArgumentException("Expression needs to start with an operation");
         }
-        Operations operation0 = operationsFactory(scanner.next());
+        Operations operation0 = getOperation(scanner.next());
         return operation0.calculate(scanner);
     }
 
-    public static Operations operationsFactory(String operationName) {
+    public static Operations getOperation(String operationName) {
         return switch (operationName) {
-            case ("sin") -> new OpSin();
-            case ("cos") -> new OpCos();
-            case ("sqrt") -> new OpSqrt();
-            case ("log") -> new OpLog();
-            case ("+") -> new OpSum();
-            case ("-") -> new OpSub();
-            case ("*") -> new OpMult();
-            case ("/") -> new OpDiv();
-            case ("pow") -> new OpPow();
+            case ("sin") -> new Operations.OpSin();
+            case ("cos") -> new Operations.OpCos();
+            case ("sqrt") -> new Operations.OpSqrt();
+            case ("log") -> new Operations.OpLog();
+            case ("+") -> new Operations.OpSum();
+            case ("-") -> new Operations.OpSub();
+            case ("*") -> new Operations.OpMult();
+            case ("/") -> new Operations.OpDiv();
+            case ("pow") -> new Operations.OpPow();
             default -> throw new IllegalArgumentException("Nonexistent operation");
         };
     }
--- Task_1_4_1/src/main/java/Operations.java
+++ Task_1_4_1/src/main/java/Operations.java
@@ -1,72 +1,88 @@
 import java.util.Scanner;
 
-abstract class Operations {
-    abstract double calculate(Scanner scanner);
+interface Operations {
+    double calculate(Scanner scanner);
+
+    default double getArg(Scanner scanner) {
+        if (!scanner.hasNext()) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
+        } else {
+            Operations operation = Calculator.getOperation(scanner.next());
+            return operation.calculate(scanner);
+        }
+    }
+
+    class OpSin implements Operations {
+        public double calculate(Scanner scan){
+            double x = getArg(scan);
+            return Math.sin(x);
+        }
+    }
+
+    class OpCos implements Operations {
+        public double calculate(Scanner scan){
+            double x = getArg(scan);
+            return Math.cos(x);
+        }
+    }
+
+    class OpSqrt implements Operations {
+        public double calculate(Scanner scan){
+            double x = getArg(scan);
+            return Math.sqrt(x);
+        }
+    }
+
+    class OpLog implements Operations {
+        public double calculate(Scanner scan){
+            double x = getArg(scan);
+            return Math.log(x);
+        }
+    }
+
+    class OpSum implements Operations {
+        public double calculate(Scanner scan){
+            double a = getArg(scan);
+            double b = getArg(scan);
+            return a + b;
+        }
+    }
+
+    class OpSub implements Operations {
+        public double calculate(Scanner scan){
+            double a = getArg(scan);
+            double b = getArg(scan);
+            return a - b;
+        }
+    }
+
+    class OpMult implements Operations {
+        public double calculate(Scanner scan){
+            double a = getArg(scan);
+            double b = getArg(scan);
+            return a * b;
+        }
+    }
+    class OpDiv implements Operations {
+        public double calculate(Scanner scan){
+            double a = getArg(scan);
+            double b = getArg(scan);
+            return a / b;
+        }
+    }
+
+    class OpPow implements Operations {
+        public double calculate(Scanner scan) {
+            double a = getArg(scan);
+            double b = getArg(scan);
+            return Math.pow(a, b);
+        }
+    }
 }
 
-class OpSin extends Operations {
-    double calculate(Scanner scan){
-        double x = Calculator.getArg(scan);
-        return Math.sin(x);
-    }
-}
 
-class OpCos extends Operations {
-    double calculate(Scanner scan){
-        double x = Calculator.getArg(scan);
-        return Math.cos(x);
-    }
-}
 
-class OpSqrt extends Operations {
-    double calculate(Scanner scan){
-        double x = Calculator.getArg(scan);
-        return Math.sqrt(x);
-    }
-}
 
-class OpLog extends Operations {
-    double calculate(Scanner scan){
-        double x = Calculator.getArg(scan);
-        return Math.log(x);
-    }
-}
-
-class OpSum extends Operations {
-    double calculate(Scanner scan){
-        double a = Calculator.getArg(scan);
-        double b = Calculator.getArg(scan);
-        return a + b;
-    }
-}
-
-class OpSub extends Operations {
-    double calculate(Scanner scan){
-        double a = Calculator.getArg(scan);
-        double b = Calculator.getArg(scan);
-        return a - b;
-    }
-}
-
-class OpMult extends Operations {
-    double calculate(Scanner scan){
-        double a = Calculator.getArg(scan);
-        double b = Calculator.getArg(scan);
-        return a * b;
-    }
-}
-class OpDiv extends Operations {
-    double calculate(Scanner scan){
-        double a = Calculator.getArg(scan);
-        double b = Calculator.getArg(scan);
-        return a / b;
-    }
-}
-
-class OpPow extends Operations {
-    double calculate(Scanner scan){
-        double a = Calculator.getArg(scan);
-        double b = Calculator.getArg(scan);
-        return Math.pow(a, b);
-    }
-}
